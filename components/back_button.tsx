4487f9c99959fb7d55e315786b375fee874e74a8
sofiamejiamuro <sofiamejiamuro@gmail.com>
2024-06-09 20:00:13 -0600

    adding styles to back button

--- components/back_button.tsx
+++ components/back_button.tsx
@@ -2,10 +2,18 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import { FaRegArrowAltCircleLeft } from "react-icons/fa";
+
 
 export default function BackButton() {
     const router = useRouter();
     return (
-        <div className='' onClick={router.back}>BackButton</div>
+        <div className='
+        text-5xl text-violet-600 px-14
+        '>
+            <button  onClick={router.back}>
+                <FaRegArrowAltCircleLeft />
+            </button>    
+        </div>
     )
 }
